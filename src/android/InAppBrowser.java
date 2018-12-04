49f9209df3d8787868a25b569299bef2dc39694a
袁家强 <1406736431@qq.com>
2018-12-04 15:54:57 +0800

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -1069,7 +1069,7 @@ public class InAppBrowser extends CordovaPlugin {
                 } catch (android.content.ActivityNotFoundException e) {
                     LOG.e(LOG_TAG, "Error dialing " + url + ": " + e.toString());
                 }
-            } else if (url.startsWith("geo:") || url.startsWith(WebView.SCHEME_MAILTO) || url.startsWith("market:") || url.startsWith("intent:") || url.startsWith("weixin:")) {
+            } else if (url.startsWith("geo:") || url.startsWith(WebView.SCHEME_MAILTO) || url.startsWith("market:") || url.startsWith("intent:") || url.startsWith("weixin:")|| url.startsWith("alipays:")) {
                 try {
                     Intent intent = new Intent(Intent.ACTION_VIEW);
                     intent.setData(Uri.parse(url));
