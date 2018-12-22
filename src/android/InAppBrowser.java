3c3971d46ab26688792beb4cb5b73fc28196b8d6
袁家强 <1406736431@qq.com>
2018-12-22 15:51:39 +0800

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -105,9 +105,9 @@ public class InAppBrowser extends CordovaPlugin {
     private static final String HIDE_URL = "hideurlbar";
     private static final String FOOTER = "footer";
     private static final String FOOTER_COLOR = "footercolor";
-    //private static final String TITLE = "title";
+    private static final String TITLE = "title";
 
-    private static final List customizableOptions = Arrays.asList(CLOSE_BUTTON_CAPTION, TOOLBAR_COLOR, NAVIGATION_COLOR, CLOSE_BUTTON_COLOR, FOOTER_COLOR);
+    private static final List customizableOptions = Arrays.asList(CLOSE_BUTTON_CAPTION, TOOLBAR_COLOR, NAVIGATION_COLOR, CLOSE_BUTTON_COLOR, FOOTER_COLOR, TITLE);
 
     private InAppBrowserDialog dialog;
     private WebView inAppWebView;
@@ -134,7 +134,7 @@ public class InAppBrowser extends CordovaPlugin {
     private boolean hideUrlBar = false;
     private boolean showFooter = false;
     private String footerColor = "";
-    //private String title = "";
+    private String title = "";
 
     /**
      * Executes the request and returns PluginResult.
@@ -629,10 +629,10 @@ public class InAppBrowser extends CordovaPlugin {
             if (footerColorSet != null) {
                 footerColor = footerColorSet;
             }            
-            /*String titleSet = features.get(TITLE);
+            String titleSet = features.get(TITLE);
             if (titleSet != null) {
                 title = titleSet;
-            }*/
+            }
         }
 
         final CordovaWebView thatWebView = this.webView;
@@ -789,7 +789,7 @@ public class InAppBrowser extends CordovaPlugin {
                 });
 
                 // Edit Text Box
-                edittext = new EditText(cordova.getActivity());
+                /*edittext = new EditText(cordova.getActivity());
                 RelativeLayout.LayoutParams textLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
                 textLayoutParams.addRule(RelativeLayout.RIGHT_OF, 1);
                 textLayoutParams.addRule(RelativeLayout.LEFT_OF, 5);
@@ -809,10 +809,10 @@ public class InAppBrowser extends CordovaPlugin {
                         }
                         return false;
                     }
-                });
+                });*/
 
                 // TITLE
-                /*TextView titletext = new TextView(cordova.getActivity());
+                TextView titletext = new TextView(cordova.getActivity());
                 RelativeLayout.LayoutParams titleLayoutParams = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
                 titleLayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
                 titletext.setLayoutParams(titleLayoutParams);
@@ -821,7 +821,7 @@ public class InAppBrowser extends CordovaPlugin {
                 titletext.setTextSize(20);
                 if (closeButtonColor != "") titletext.setTextColor(android.graphics.Color.parseColor(closeButtonColor));
                 titletext.setGravity(android.view.Gravity.CENTER_VERTICAL);
-                titletext.setPadding(this.dpToPixels(10), 0, this.dpToPixels(10), 0);*/
+                titletext.setPadding(this.dpToPixels(10), 0, this.dpToPixels(10), 0);
 
                 // Header Close/Done button
                 View close = createCloseButton(5);
@@ -950,8 +950,8 @@ public class InAppBrowser extends CordovaPlugin {
 
                 // Add the views to our toolbar if they haven't been disabled
                 if (!hideNavigationButtons) toolbar.addView(actionButtonContainer);
-                if (!hideUrlBar) toolbar.addView(edittext);
-                //if (title != "") toolbar.addView(titletext); 
+                //if (!hideUrlBar) toolbar.addView(edittext);
+                if (title != "") toolbar.addView(titletext); 
 
                 // Don't add the toolbar if its been disabled
                 if (getShowLocationBar()) {
